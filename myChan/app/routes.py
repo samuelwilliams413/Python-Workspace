465d68991eeaf51e3ab66921903e605af4ba7acc
samuelwilliams413 <samuelwilliams413@gmail.com>
2018-08-04 22:05:52 +1000

    Flask post implimented
    python isn't a real language

--- myChan/app/routes.py
+++ myChan/app/routes.py
@@ -1,17 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username' : "SMW"}
-    return '''
-    <html>
-        <head>
-            <title>Home Page - SMW myChan</title>
-        </head>
-        <body> 
-            <h1> Hello ''' + user['username'] + '''!</h1>
-        </body>
-                
-    </html>
-    '''
+    posts = [
+        {
+            'author' : {'username' : "Alice"},
+            'body' : 'Post A'
+        },
+        {
+            'author' : {'username' : "Bob"},
+            'body' : 'Post B'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
