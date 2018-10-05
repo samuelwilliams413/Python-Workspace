2c11151c52f2f625f8195ffaacd8172baacaa31f
samuelwilliams413 <samuelwilliams413@gmail.com>
2018-10-05 16:11:10 +1000

    Nastrond: levels
    Completed: Part 11 - Levels, floors
    http://rogueliketutorials.com/libtcod/11

--- Nastrond/entity.py
+++ Nastrond/entity.py
@@ -6,11 +6,8 @@ from render_functions import RenderOrder
 
 
 class Entity:
-    """
-    A generic object to represent players, enemies, items, etc.
-    """
     def __init__(self, x, y, char, color, name, blocks=False, render_order=RenderOrder.CORPSE, fighter=None, ai=None,
-                 item=None, inventory=None):
+                 item=None, inventory=None, stairs=None, level=None):
         self.x = x
         self.y = y
         self.char = char
@@ -22,6 +19,8 @@ class Entity:
         self.ai = ai
         self.item = item
         self.inventory = inventory
+        self.stairs = stairs
+        self.level = level
 
         if self.fighter:
             self.fighter.owner = self
@@ -34,6 +33,12 @@ class Entity:
 
         if self.inventory:
             self.inventory.owner = self
+
+        if self.stairs:
+            self.stairs.owner = self
+
+        if self.level:
+            self.level.owner = self
 
     def move(self, dx, dy):
         # Move the entity by a given amount
